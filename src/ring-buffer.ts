12f8e441f5b76419531e8af71c0242063a27fd65
kakashi1210 <desmondaditya@gmail.com>
2024-01-04 19:17:21 +0530

    feat: added function for enqueue, deque and state

---
+++ src/ring-buffer.ts
@@ -0,0 +1,48 @@
+export class RingBuffer<T> {
+  public capacity: number;
+  private head: number;
+  private tail: number;
+  private circularArray: T[] | number[];
+
+  constructor(capacity: number) {
+    this.capacity = capacity;
+    this.circularArray = [];
+    this.head = this.tail = -1;
+  }
+
+  enqueue(data: T): void {
+    if (this.tail === -1) {
+      this.head = this.tail = 0;
+      this.circularArray[this.tail] = data;
+      return;
+    }
+
+    if (this.tail >= this.capacity - 1) {
+      this.tail = 0;
+      this.circularArray[this.tail] = data;
+      return;
+    }
+    this.tail++;
+    this.circularArray[this.tail] = data;
+    return;
+  }
+
+  deque(): void {
+    if (this.head === -1) {
+      throw new Error("Cannot Deque Empty Buffer");
+    }
+
+    this.circularArray[this.head] = -1;
+
+    if (this.head === this.capacity - 1) {
+      this.head = 0;
+    } else {
+      this.head++;
+    }
+    return;
+  }
+
+  state(): void {
+    console.log(this.circularArray, this.head, this.tail);
+  }
+}
